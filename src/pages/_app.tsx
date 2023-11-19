5b37c282edeef05ec6e4454da9fdf5cbdf79d983
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-19 04:07:51 +0000

    Errors with `pages` router

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import { GeistSans } from 'geist/font/sans'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={GeistSans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold tracking-tight">
+        this text uses geist font, errors errors :(
+      </h1>
+      <p className="text-2xl font-semibold text-neutral-400">
+        ver. geist@1.1.0
+      </p>
+    </>
+  )
+}
